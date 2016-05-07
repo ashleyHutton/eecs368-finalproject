7d951ddbca8292bc88f32ab97f8f86c0ba880b3d
Ashley Hutton <ashley.hutton@ku.edu>
2016-05-06 19:04:16 -0500

    Fix bug in session.php where we grab user id

--- FinalProjectOfficial/session.php
+++ FinalProjectOfficial/session.php
@@ -10,7 +10,7 @@
 	//echo implode(" ",$row);
 	//echo $row['username'];
 	$login_session = $row['UserName'];
-	$user_id = $row['ID'];
+	$sql = mysqli_query($db,"SELECT ID FROM BlogUsers WHERE UserName = '$login_session' ");
 	//isset determines if a variable is set and not null
 	//If it is null it sends user to the login page
 	if(!isset($_SESSION['login_user'])){
