98120d7061fb4f6a8f38b944381d566e9c94af08
Chris Bernosky <chris.bernosky.1994@gmail.com>
2016-05-11 18:22:21 -0500

    created like button

--- FinalProjectOfficial/post_loader.php
+++ FinalProjectOfficial/post_loader.php
@@ -1,34 +1,48 @@
 <?php
 session_start();
-include("config.php");
-
+include("session.php");
 if(isset($_POST['ID_filter'])){ //Here we check if we are selecting only a certain user id posts
 	$filter_id = $_POST['ID_filter'];
-	$postQuery = "SELECT author_id,content FROM Posts WHERE author_id = '$filter_id'";
+	$postQuery = "SELECT author_id,content,post_id FROM Posts WHERE author_id = '$filter_id'";
 }else{
-	$postQuery = "SELECT author_id,content FROM Posts";
+	$postQuery = "SELECT author_id,content,post_id FROM Posts";
 }
-
+$likeQuery = "SELECT post_id FROM post_like WHERE (uid = $user_id)";
 $fetched_posts = array();
-
 if($result = $db->query($postQuery)){
 	while($row = $result->fetch_assoc()){
-		array_push($fetched_posts, [$row['author_id'],$row['content']]);
+		array_push($fetched_posts, [$row['author_id'],$row['content'],$row['post_id']]);
 	}
 	$result->free();
 }
-
+$fetched_likes = array();
+if($result = $db->query($likeQuery)){
+	while($row = $result->fetch_assoc()){
+		array_push($fetched_likes, $row['post_id']);
+	}
+	$result->free();
+}
 for($i = 0; $i < sizeof($fetched_posts); $i++){
 	$posterId = $fetched_posts[$i][0];
 	$postCont = $fetched_posts[$i][1];
+	$thisPostID = $fetched_posts[$i][2];
+	if(in_array($thisPostID,$fetched_likes)){
+		$vote = "-";
+	}else{
+		$vote = "+";
+	}
+	echo $user_id;
 	$formated_post = "
 	<div class='post_area'>
 		<div>Author: <label>$posterId</label></div>
 		<div class='post_content'>
 			$postCont
+			<div class = 'buttons'>
+			<button type= 'submit' name= 'add_vote' id = 'post_id'> ". $vote ."</button>
 		</div>
+		</div>
+		
 	</div>";
 	echo $formated_post;
 }
-
 ?>
